7e222ae74a7a89e0b032d8fddd8ee7f24a6121bd
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-12-04 00:03:14 +0600

    added intro page with divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-devider";
 
 export default function Home() {
   return (
     <main className="">
-
+      <Intro></Intro>
+      <SectionDivider></SectionDivider>
     </main>
   )
 }
